4d5e4607a26206076b8b5376e75152bc038a13af
Damien <damien.pellet94@hotmail.fr>
2022-06-14 17:24:20 +0200

    wip

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,7 +7,6 @@ function photographerFactory(data) {
         const prix = document.createElement('h6');
         const tag = document.createElement('h5');
         const h3 = document.createElement('h3');
-        const pays = document.createElement('h3');
         const h2 = document.createElement( 'h2' );
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
@@ -15,7 +14,9 @@ function photographerFactory(data) {
 
 
         h2.textContent = name;
-        h3.textContent = city;
+        h2.setAttribute("aria-label", `${name}`)
+        h2.setAttribute("role", "link")
+        h3.textContent = city + ', ' + country;
         // pays.textContent = country;
         tag.textContent = tagline;
         prix.textContent = price;
@@ -23,7 +24,7 @@ function photographerFactory(data) {
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
-        article.appendChild(pays);
+        // article.appendChild(pays);
         article.appendChild(tag);
         article.appendChild(prix);
 
